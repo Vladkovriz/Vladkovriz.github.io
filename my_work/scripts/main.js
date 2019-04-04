ec5f1aaa3c310ea213b6892bcf3e6e465dfa339d
Vladyslav <vladyslav.hrytsenko2@nure.ua>
2019-04-04 18:29:19 +0300

    Auto stash before merge of "master" and "origin/master"
    Add "BLOW" project to my portfolio

---
+++ my_work/scripts/main.js
@@ -0,0 +1,28 @@
+var testis = [
+  {
+    name: "Steve Jobs",
+    image: "background-image: url(img/1.jpg);",
+    position: "Founder Lucoil",
+    descr: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto culpa corporis vel minima aspernatur, voluptatem voluptas quis voluptatibus dolore odio in dolores libero dolorum corrupti!"
+  },
+  {
+    name: "Steve Jobs2",
+    image: "background-image: url(img/2.jpg);",
+    position: "CEO",
+    descr: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto culpa corporis vel minima aspernatur, voluptatem voluptas quis voluptatibus dolore odio in dolores libero dolorum corrupti!"
+  },
+  {
+    name: "Steve Jobs3",
+    image: "background-image: url(img/3.jpg);",
+    position: "CEO",
+    descr: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto culpa corporis vel minima aspernatur, voluptatem voluptas quis voluptatibus dolore odio in dolores libero dolorum corrupti!"
+  }
+]
+
+
+new Vue({
+  el: '#app',
+  data: {
+    testis: testis
+  }
+})
